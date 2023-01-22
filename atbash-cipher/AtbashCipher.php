795e4c4ff8a04a28699146273ff224c22f9a0252
Matthew Baillie <mbaillie93@gmail.com>
2023-01-22 12:23:52 -0800

    Atbash cipher complete

--- atbash-cipher/AtbashCipher.php
+++ atbash-cipher/AtbashCipher.php
@@ -26,9 +26,16 @@ declare(strict_types=1);
 
 function encode(string $text): string
 {
+    $cleanedString = strtolower(preg_replace('/[^a-z0-9]/i', '', $text));
     // define alphabet
+    $alphabet = "abcdefghijklmnopqrstuvwxyz";
+    $revAlphabet = strrev($alphabet);
+    $replacedString = strtr($cleanedString, $alphabet, $revAlphabet);
+    $chuncked = trim(chunk_split($replacedString, 5, ' '), ' ');
+    echo $chuncked;
     // reverse alphabet
     // use preg_replace with reg expression (selecting only letters)
     // then swap the letters
-    throw new \BadFunctionCallException("Implement the encode function");
+    return $chuncked;
 }
+encode("OMG");
